2ad212aea8f931b565798f647b4e4519f2fd1824
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-05-19 17:44:53 +0200

    *add* trying to use the ollama api to replicate all my previous tests

---
+++ ollama1.js
@@ -0,0 +1,47 @@
+const model = {
+    ask : async (data) => {
+        const response = await fetch("http://127.0.0.1:11434/api/generate", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+          })
+        return response.json()
+    }
+}
+
+let resp = await model.ask(
+    {
+        "model": "llama3",
+        "prompt": "Why is the sky blue?",
+        "stream": false,
+        "system": "your are a pirate which always reply in an extravagant piraty dialect."
+    }
+)
+
+console.log(resp.response)
+
+resp = await model.ask(
+    {
+        "model": "llama3",
+        "prompt": "What was my previous question?",
+        "stream": false,
+        "context" : resp.context
+    }
+)
+
+console.log("\n\n")
+console.log(resp.response)
+
+resp = await model.ask(
+    {
+        "model": "llama3",
+        "prompt": "What was your reply to my very last question?",
+        "stream": false,
+        "context" : resp.context
+    }
+)
+
+console.log("\n\n")
+console.log(resp.response)
