e194b1b07d6323ace033520e025aa687de98d9e4
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-05-22 17:00:19 +0200

    *edit* ollama2code now use dolphin llama3

--- ollama1.js
+++ ollama1.js
@@ -8,12 +8,24 @@ const model = {
             body: JSON.stringify(data),
           })
         return response.json()
-    }
+    },
+    embeddings : async (data) => {
+        const response = await fetch("http://127.0.0.1:11434/api/embeddings", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+          })
+        return response.json()
+    },
 }
+
+const models = ["llama3", "dolphin-llama3:8b-256k"]
 
 let resp = await model.ask(
     {
-        "model": "llama3",
+        "model": models[1],
         "prompt": "Why is the sky blue?",
         "stream": false,
         "system": "your are a pirate which always reply in an extravagant piraty dialect."
@@ -24,7 +36,7 @@ console.log(resp.response)
 
 resp = await model.ask(
     {
-        "model": "llama3",
+        "model": models[1],
         "prompt": "What was my previous question?",
         "stream": false,
         "context" : resp.context
@@ -36,7 +48,7 @@ console.log(resp.response)
 
 resp = await model.ask(
     {
-        "model": "llama3",
+        "model": models[1],
         "prompt": "What was your reply to my very last question?",
         "stream": false,
         "context" : resp.context
@@ -46,3 +58,12 @@ resp = await model.ask(
 console.log("\n\n")
 console.log(resp.response)
 console.log(resp.context)
+
+
+resp = await model.embeddings({
+    "model": "nomic-embed-text",
+    "prompt": "What was my previous question?",
+    "stream": false,
+})
+console.log("\n\nEMBEDDINGS")
+console.log(resp.embedding)
