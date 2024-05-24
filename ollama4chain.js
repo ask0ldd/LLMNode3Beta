36b2f59567128efbd9e22007f0ee333d27147712
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-05-24 02:15:53 +0200

    *edit* chaining llm requests

--- ollama4chain.js
+++ ollama4chain.js
@@ -11,11 +11,18 @@ model.setContextSize = 0
 
 const montecristoChapter1 = fs.readFileSync('docs/montecristo.txt', "utf8")
 
-const resp = await model.ask("Summarize the following text in 30 lines : '''" + montecristoChapter1.substring(0, 7800-1) + "'''.")
+/*const resp = await model.ask("Summarize the following text in 30 lines : '''" + montecristoChapter1.substring(0, 7800-1) + "'''.")
 
 console.log(resp.response)
 
 model.setContext(resp.context)
 const resp2 = await model.ask("Summarize the context into 5 lines.")
 
-console.log("\n\n --- \n\n" + resp2.response)
+console.log("\n\n --- \n\n" + resp2.response)*/
+
+model.ask("Summarize the following text in 30 lines : '''" + montecristoChapter1.substring(0, 7800-1) + "'''.")
+.then(async response => {
+    model.setContext(response.context)
+    const resp2 = await model.ask("Summarize the context into 5 lines.")
+    console.log("\n\n --- \n\n" + resp2.response)
+})
