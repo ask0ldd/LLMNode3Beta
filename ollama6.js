8f7a5031711ef805e512f700295ce2c1be013c72
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-05-25 18:06:56 +0200

    *add* ollama6 js chaining capabilities testing

---
+++ ollama6.js
@@ -0,0 +1,77 @@
+import { model } from "./AIModel.js";
+import * as fs from "fs"
+import { ChromaClient } from "chromadb"
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter"
+
+function splitTextToSequences(text, seqLength){
+    const words = text.split(/\s+/)
+    let sequences = []
+    let sequence = []
+    for(let i = 0; i < words.length; i++){
+        sequence.push(words[i])
+        if(i == 0) continue
+        if(i % seqLength == 0 || i === words.length - 1){
+            sequences.push(sequence.join(" "))
+            sequence = []
+        }
+    }
+    return sequences
+}
+
+async function stringToSplitDocs(string){
+    const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1500, chunkOverlap: 200, separators : ' ' })
+    const docs = await textSplitter.createDocuments([string])
+    return docs
+}
+
+
+const montecristo = fs.readFileSync('docs/toserveman.txt', "utf8")
+const chunkyMontecristo = splitTextToSequences(montecristo, 200)
+/*const docs = await stringToSplitDocs(montecristo)
+const chunkyMontecristo = docs.map(doc => doc.pageContent)*/
+
+let embeddings = []
+let ids = []
+let index = 1
+for(const chunk of chunkyMontecristo){
+    const response = await model.embeddings(chunk)
+    embeddings.push(response.embedding)
+    ids.push('id'+index)
+    index++
+}
+
+const chromaClient = new ChromaClient()
+await chromaClient.deleteCollection({name : "mycollection"})
+const collection = await chromaClient.createCollection({name : "mycollection", metadata: { "hnsw:space": "cosine" },})
+
+await collection.add({ids : ids, embeddings: embeddings, documents: chunkyMontecristo})
+
+/*for(const chunk of chunkyMontecristo){
+    // console.log(chunk)
+    const response = await model.embeddings(chunk)
+    await collection.add({ids : ['id' + index], embeddings: response.embedding, documents: [chunk]})
+    index++
+}*/
+
+console.log(chunkyMontecristo[0])
+
+const queryEmbedding = await model.embeddings("what is the real use of the alien book?")
+// console.log(queryEmbedding.embedding)
+const ragResults = await collection.query({queryEmbeddings : queryEmbedding.embedding, nResults:3});
+console.log("\n\n\u001b[1;33m " + ragResults.documents[0][0]);
+console.log("\n\n\u001b[1;35m " + ragResults.documents[0][1]);
+console.log("\u001b[1;34m " + ragResults.ids);
+console.log(ragResults.distances)
+
+
+model.setModel("dolphin-llama3:8b-256k")
+model.setSystemPrompt(`You are a helpful assistant. Avoid repeting the same pieces of information or the same facts multiple times. Give emphasis to the parts that stand out. Use a descriptive journalistic tone. You are not a novelist but a scholar writing essays for a living.`)
+model.setContextSize = 22000
+
+const resp = await model.ask(`extract those those three chunks of text and rewrite them into one global text : \n\n''' {'chunk1' : '${ragResults.documents[0][0]}', \n\n 'chunk2': '${ragResults.documents[0][1]}', \n\n 'chunk3' : '${ragResults.documents[0][2]}'} '''`)
+
+console.log("\u001b[1;34m " + resp.response)
+
+const resp2 = await model.ask(`use the following text only to tell me what the real motivation of the aliens is : \n\n ${resp.response}`)
+
+console.log("\u001b[1;31m " + resp2.response)
