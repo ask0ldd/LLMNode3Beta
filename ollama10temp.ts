1b24ed732901b6e1a319b10287469a11c06b0187
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-05-31 21:46:58 +0200

    *edit* fixing sequential requests

--- ollama10temp.ts
+++ ollama10temp.ts
@@ -24,24 +24,29 @@ function splitTextToSequences(text : string, seqLength : number = 200) : Array<s
 
 const model = new AIModel({})
 
+// You are an helpful assistant. Your reply should always follow the json schema given by the user. No need for any commentary or explaination. Calculate the result value of all formulas.
+
 model.setModel("dolphin-llama3:8b-256k")
-model.setSystemPrompt(`You are an helpful assistant. Your reply should always follow the schema given by the user. No need for any commentary or explaination. Only pure datas in the json format.`)
+model.setSystemPrompt(`You are an helpful assistant. Your reply should always follow given schema. No need for any commentary or explaination. Calculate all the values delivered into the json.`)
 model.setTemperature(0)
 
 await model.setContextSize(8000)
 
-const resp = await model.ask('Your total happiness should start at twenty\n\n schema : {"total_happiness": value}')
+const resp = await model.ask(`Your total happiness should start at 20\n\n
+schema : { "total_happiness": value, "power2" : schema.total_happiness*schema.total_happiness }`)
 
 console.log("\u001b[1;34m " + resp.response)
 
 model.setContext(resp.context as number[])
 
-const resp2 = await model.ask('You lose 2 points of happiness, what is your total happiness right now?\n\n schema : {"total_happiness": value}')
+const resp2 = await model.ask(`You lose 2 points of happiness, what is your new total happiness right now?\n\n
+update this schema : { "total_happiness": value, "power2" : schema.total_happiness*schema.total_happiness }`)
 
 console.log("\u001b[1;35m " + resp2.response)
 
 model.setContext(resp2.context as number[])
 
-const resp3 = await model.ask('You lose 2 points of happiness, what is your total happiness right now?\n\n schema : {"total_happiness": value}')
+const resp3 = await model.ask(`You lose 2 points of happiness, what is your new total happiness right now?
+\n\n update this schema : { "total_happiness": value, "power2" : schema.total_happiness*schema.total_happiness }`)
 
 console.log("\u001b[1;31m " + resp3.response)
