afff542ebb57475808deb8a45e3a204bea6d68f0
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-06-01 18:39:28 +0200

    *edit* ollama10temp trying to get fn calling responsive

--- ollama10temp.ts
+++ ollama10temp.ts
@@ -27,26 +27,62 @@ const model = new AIModel({})
 // You are an helpful assistant. Your reply should always follow the json schema given by the user. No need for any commentary or explaination. Calculate the result value of all formulas.
 
 model.setModel("dolphin-llama3:8b-256k")
-model.setSystemPrompt(`You are an helpful assistant. Your reply should always follow given schema. No need for any commentary or explaination. Calculate all the values delivered into the json.`)
+model.setSystemPrompt(`You are an helpful assistant. Your reply should always follow the last given schema. No need for any commentary or explaination. All values in a json object should be a mathematical value, no operation.`)
 model.setTemperature(0)
 
 await model.setContextSize(8000)
 
-const resp = await model.ask(`Your total happiness should start at 20\n\n
-schema : { "total_happiness": value, "power2" : schema.total_happiness*schema.total_happiness }`)
-
+model.setContext([])
+const resp = await model.ask(`Your total happiness should start at 20 and never go past beyond that; no matter what the instruction is.\n\n
+schema :\n\n
+{
+    "total_happiness": value, 
+    "power2" : schema.total_happiness*schema.total_happiness
+}`)
 console.log("\u001b[1;34m " + resp.response)
 
 model.setContext(resp.context as number[])
-
-const resp2 = await model.ask(`You lose 2 points of happiness, what is your new total happiness right now?\n\n
-update this schema : { "total_happiness": value, "power2" : schema.total_happiness*schema.total_happiness }`)
-
+const resp2 = await model.ask(`You lose 2 points of happiness, what is your new total happiness?\n\n
+update this schema :\n\n
+{
+    "total_happiness": value, 
+    "power2" : schema.total_happiness*schema.total_happiness
+}`)
 console.log("\u001b[1;35m " + resp2.response)
 
 model.setContext(resp2.context as number[])
-
-const resp3 = await model.ask(`You lose 2 points of happiness, what is your new total happiness right now?
-\n\n update this schema : { "total_happiness": value, "power2" : schema.total_happiness*schema.total_happiness }`)
-
+const resp3 = await model.ask(`You lose 2 points of happiness, what is your new total happiness?\n\n
+update this schema :\n\n
+{
+    "total_happiness": value, 
+    "power2" : schema.total_happiness*schema.total_happiness
+}`)
 console.log("\u001b[1;31m " + resp3.response)
+model.setContext(resp3.context as number[])
+
+const resp4 = await model.ask(`You gain 8 points of happiness, what is your new total happiness?\n\n
+update this schema :\n\n
+{
+    "total_happiness": value, 
+    "power2" : schema.total_happiness*schema.total_happiness
+}`)
+console.log("\u001b[1;32m " + resp4.response)
+
+model.setContext(resp4.context as number[])
+const resp5 = await model.ask(`add a new third property named anger with a value of 'false' at the end of the last schema.\n\n
+example :\n\n
+{
+    "total_happiness": value, 
+    "power2" : schema.total_happiness*schema.total_happiness,
+    "anger" : boolean
+}
+`)
+console.log("\u001b[1;33m " + resp5.response)
+
+model.setContext([])
+model.setSystemPrompt('You are a helpful assistant. Only reply with numerical values.')
+const resp6 = await model.ask(`here is a json object :\n\n
+${resp5.response}\n\n
+what is the value of power2 ?`)
+
+console.log("\u001b[1;33m " + resp6.response)
