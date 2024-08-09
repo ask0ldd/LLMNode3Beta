11507a1a964090a42facbb4c769c3ea43259f878
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-08-09 05:17:37 +0200

    *edit* llm reformating html text with missing \n

--- ollama13.ts
+++ ollama13.ts
@@ -1,8 +1,4 @@
-import * as fs from "fs"
-import { ChromaClient } from "chromadb"
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter"
-import { Document, BaseDocumentTransformer } from "@langchain/core/documents"
-import { AIModel, Embedding } from "./AIModel.js"
+import { AIModel } from "./AIModel.js"
 import { search, SafeSearchType } from 'duck-duck-scrape'
 import * as cheerio from 'cheerio'
 
@@ -27,6 +23,7 @@ const model = new AIModel({})
 
 // You are an helpful assistant. Your reply should always follow the json schema given by the user. No need for any commentary or explaination. Calculate the result value of all formulas.
 
+/*
 model.setModel("llama3.1:8b")
 model.setSystemPrompt(`You are a revered story teller, as great as lovecraft or edgar alan poe.`)
 model.setTemperature(0.1)
@@ -36,6 +33,7 @@ await model.setContextSize(8000)
 model.setContext([])
 const resp = await model.ask(`Write for me a short story where, in the future, people leaves in such small appartments that they need to get a virtual reality subscription and a headset to escape the induced claustrophobia. View this as the final step of the exploitation of mankind.`) 
 console.log("\u001b[1;33m " + resp.response)
+*/
 
 const searchResults = await search('node.js', {
     safeSearch: SafeSearchType.STRICT
@@ -62,3 +60,13 @@ try {
 const webpage = cheerio.load(html)
 
 console.log('main : ', webpage('main').text())
+
+model.setModel("llama3.1:8b")
+model.setSystemPrompt(`You are a expert linguist.`)
+model.setTemperature(0.1)
+
+await model.setContextSize(8000)
+
+model.setContext([])
+const resp = await model.ask(`Take the following text, and for each capital caracter, ask yourself if it should lead to a new line. If it does, then add a new line to the text. In the end, output the modified text. Nothing more.\n\n${webpage('main').text().trim()}`) 
+console.log("\u001b[1;33m " + resp.response)
