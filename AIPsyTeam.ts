2892b4005890c34099907ea776bee93975402c06
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-08-13 21:00:23 +0200

    *edit* fixing the skillset and the question generator agents

--- AIPsyTeam.ts
+++ AIPsyTeam.ts
@@ -1,5 +1,5 @@
 import { AIAgent } from "./AIAgent.js";
-import { conversationistPrompt, skillRankingPrompt, skillsetGeneratorPrompt } from "./AIPsyPrompts.js";
+import { conversationistPrompt, jobExtractorPrompt, skillRankingPrompt, skillsetGeneratorPrompt, skillToQuestionsPrompt } from "./AIPsyPrompts.js";
 
 class AISkillsetGeneratorAgent extends AIAgent{
     #job : string | undefined = undefined
@@ -10,24 +10,28 @@ class AISkillsetGeneratorAgent extends AIAgent{
 
     override async call(iter : number = 0) : Promise<string>{
         let currentIter = iter
-        if(this.#request == "") throw new Error("Request is missing.")
-        const response = await this.#model.ask(this.#request)
-        this.#log(response.response)
-        this.#lastOutput = response.response
-        if(this.#regexValidator == undefined) return response.response
-        if(this.checkOutputValidity(response.response, this.#regexValidator)) return response.response
-        if(currentIter+1 < this.#maxIter) return this.call(currentIter + 1)
-        throw new Error(`Couldn't format the reponse the right way despite the ${this.#maxIter} iterations.`)
+        if(this.getRequest() == "") throw new Error("Request is missing.")
+        const response = await this.getModel().ask(this.getRequest())
+        this.getLog()(response.response)
+        this.setLastOutput(response.response)
+        if(this.getRegexValidator() == undefined) return response.response
+        if(this.checkOutputValidity(response.response, this.getRegexValidator() as RegExp)) return response.response
+        if(currentIter+1 < this.getMaxIter()) return this.call(currentIter + 1)
+        throw new Error(`Couldn't format the reponse the right way despite the ${this.getMaxIter()} iterations.`)
     }
 }
 
 export class AIPsyTeam{
+
+    static jobExtractorAgent = new AIAgent("Job Extractor Agent").resetContext()
+    .setSystemPrompt(jobExtractorPrompt)
+
     // !!! should mix own knowledge with whats found only?
-    static requiredSkillsetGeneratorAgent = new AISkillsetGeneratorAgent("Skillset Generator").resetContext()
+    static requiredSkillsetGeneratorAgent = new AIAgent("Skillset Generator").resetContext()
     .setSystemPrompt(skillsetGeneratorPrompt)
 
-    static skillToQuestionTranslatorAgent = new AIAgent("Skill To Question Translator Agent").resetContext()
-    .setSystemPrompt(skillToQuestionPrompt)
+    static skillToQuestionsTranslatorAgent = new AIAgent("Skill To Question Translator Agent").resetContext()
+    .setSystemPrompt(skillToQuestionsPrompt)
 
     static conversationistAgent = new AIAgent("Conversationist").resetContext()
     .setSystemPrompt(conversationistPrompt)
