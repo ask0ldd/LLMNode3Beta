29dad7e09c01e98e77a8839ae4c80de2d8a72006
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-08-15 05:03:54 +0200

    *edit* ollama26 : answers spectrum rating / one on one comparison rating

--- ollama26.ts
+++ ollama26.ts
@@ -1,10 +1,11 @@
+import { answersSpectrumExample, technicalLeadershipQuestionsExample } from "./AIPsyPrompts.js";
 import { AIPsyTeam } from "./AIPsyTeam.js";
 
 const techLeadPrompt = "Tech Lead in a the company's department dealing with the frontend of a social media plateform"
-const DataScientist = "Data Scientist in charge of analyzing NASDAQ trends"
+const DataScientistPrompt = "Data Scientist in charge of analyzing NASDAQ trends"
 
 const jobDisambiguation = await AIPsyTeam.jobExtractorAgent
-    .setRequest(DataScientist)
+    .setRequest(techLeadPrompt)
     .call()
 
 console.log('\n\n job title : ' + JSON.parse(jobDisambiguation).jobTitle)
@@ -53,3 +54,45 @@ const rankedQuestions = await AIPsyTeam.skillAssessmentQuestionsRankingAgent
     .call()
 
 console.log('\n\n ranked questions : ' + rankedQuestions)
+
+const question1 = JSON.parse(rankedQuestions)[0]
+
+const answersSpectrum = await AIPsyTeam.skillAssessmentQuestion_TenAnswersSpectrumProducerAgent
+        .setRequest(`Here is the specified position :\n
+            ${JSON.parse(jobDisambiguation).jobTitle}\n\n
+            Here is the given question :\n
+            ${question1}\n\n
+            `)
+        .call()
+
+const rating = await AIPsyTeam.oneOnOneAnswersComparisonAgent
+    .setRequest(
+        `Here is the given question :\n
+        ${technicalLeadershipQuestionsExample[0]}\n\n
+        Here is the reference answer :\n
+        ${answersSpectrumExample[5].answer}\n\n
+        Here is the candidate answer :\n
+        I would recommend performing a thorough code review of the existing codebase. Following this, I'd suggest implementing a contemporary frontend framework, with React or Angular being prime candidates. To improve the overall structure, a modular architecture could be beneficial. For managing styles more effectively, we might consider adopting a CSS-in-JS approach, such as Styled Components.
+        `).call()//.loopuntil
+
+const improvementsNeeded = await AIPsyTeam.compareToPerfectAnswerAgent
+.setRequest(
+    `Here is the given question :\n
+    ${technicalLeadershipQuestionsExample[0]}\n\n
+    Here is the reference answer :\n
+    ${answersSpectrumExample[0].answer}\n\n
+    Here is the candidate answer :\n
+    I would recommend performing a thorough code review of the existing codebase. Following this, I'd suggest implementing a contemporary frontend framework, with React or Angular being prime candidates. To improve the overall structure, a modular architecture could be beneficial. For managing styles more effectively, we might consider adopting a CSS-in-JS approach, such as Styled Components.
+`).call()//.loopuntil
+
+
+
+
+
+
+
+
+// console.log('\n\n answers spectrum : ' + answersSpectrum)
+// console.table(JSON.parse(answersSpectrum))
+
+// console.log(`\n\nrating : ${rating}\nquestion : ${technicalLeadershipQuestionsExample[0]}\nref : ${answersSpectrumExample[0].answer}\ncandidate : I would recommend performing a thorough code review of the existing codebase. Following this, I'd suggest implementing a contemporary frontend framework, with React or Angular being prime candidates. To improve the overall structure, a modular architecture could be beneficial. For managing styles more effectively, we might consider adopting a CSS-in-JS approach, such as Styled Components.\n`)
