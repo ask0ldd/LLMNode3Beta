90565b1144afa3c66d5065a5c0caf279e09e2cab
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-08-19 16:57:06 +0200

    *edit* ai chain : resolution methods added

--- AIChain.ts
+++ AIChain.ts
@@ -3,16 +3,26 @@ import { AIAgent } from "./AIAgent";
 class AIChain{
     agents : AIAgent[] = []
     #index = 0
+    state = {}
 
     pushAgent(agent : AIAgent){
         this.agents.push(agent)
     }
 
-    next(value : string){
+    async next(previousAgenceResponse?: string){
         this.#index++
+        const response = await this.agents[this.#index].rawCall()
+        console.log(JSON.stringify(response))
     }
 
     restart(){
         this.#index = 0
+    }
+
+    resolve(){
+        this.restart()
+        while(this.#index++ < this.agents.length){
+            this.next()
+        }
     }
 }
