6d5ad93a4c89574a70c845ec7fd5796e9eb13375
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-08-19 16:58:18 +0200

    *edit* ai psy team : rawcall method replacing call

--- AIPsyTeam.ts
+++ AIPsyTeam.ts
@@ -8,7 +8,7 @@ class AISkillsetGeneratorAgent extends AIAgent{
         this.#job = job
     }
 
-    override async call(iter : number = 0) : Promise<string>{
+    override async rawCall(iter : number = 0) : Promise<string>{
         let currentIter = iter
         if(this.getRequest() == "") throw new Error("Request is missing.")
         const response = await this.getModel().ask(this.getRequest())
@@ -16,9 +16,11 @@ class AISkillsetGeneratorAgent extends AIAgent{
         this.setLastOutput(response.response)
         if(this.getRegexValidator() == undefined) return response.response
         if(this.checkOutputValidity(response.response, this.getRegexValidator() as RegExp)) return response.response
-        if(currentIter+1 < this.getMaxIter()) return this.call(currentIter + 1)
+        if(currentIter+1 < this.getMaxIter()) return this.rawCall(currentIter + 1)
         throw new Error(`Couldn't format the reponse the right way despite the ${this.getMaxIter()} iterations.`)
     }
+
+    call = this.rawCall
 }
 
 export class AIPsyTeam{
