bdc442ca8eb400fe32c3d27aaec5580f7d7f95f3
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-08-26 02:33:26 +0200

    *edit* AIRagTools : init chromadb and add embeddings to a collection methods

--- AIRagTools.ts
+++ AIRagTools.ts
@@ -1,9 +1,27 @@
 import * as fs from "fs"
-import { ChromaClient } from "chromadb"
+import { ChromaClient, Collection, Embedding } from "chromadb"
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter"
 import { AIModel } from "./AIModel"
 
 export class AIRAGTools{
+
+    static #chromaClient : ChromaClient = new ChromaClient()
+    static #chromeCollections : {name : string, collection : Collection}[]= []
+
+    static async initChromaCollection(collectionName : string) {
+        const collection = await this.#chromaClient.createCollection({name : collectionName, metadata: { "hnsw:space": "cosine" },})
+        this.#chromeCollections.push({name : collectionName, collection })
+    }
+
+    static async addToChromaCollection(collectionName : string, embeddingObj : { ids : string[], embeddings : Embedding[], documentChunks : string[]}) {
+        const collection = this.#chromeCollections.find(collection => collection.name == collectionName)?.collection
+        if(collection) await collection.add({ids : embeddingObj.ids, embeddings: embeddingObj.embeddings, documents: embeddingObj.documentChunks})
+    }
+
+    /*
+    await chromaClient.deleteCollection({name : "mycollection"})
+    const collection = await chromaClient.createCollection({name : "mycollection", metadata: { "hnsw:space": "cosine" },})
+    await collection.add({ids : ids, embeddings: embeddings, documents: chunkyMontecristo})*/
 
     static splitStringToEmbeddableChunks(text : string, seqLength : number) : string[] {
         const words = text.split(/\s+/)
@@ -30,18 +48,17 @@ export class AIRAGTools{
         }
     }
 
-    static async splitAndEmbedUtf8TxtFile(filePath : string, model : AIModel, seqLength = 200){
+    static async splitAndEmbedUtf8TxtFile(filePath : string, model : AIModel, seqLength = 200) : Promise<Embedding[]>{
         const chunks = this.splitUtf8TxtFileToEmbeddableChunks(filePath, seqLength)
         let embeddings = []
         let ids = []
         let index = 1
         for(const chunk of chunks){
             const response = await model.embeddings(chunk)
-            embeddings.push(response.embedding)
+            embeddings.push(response.embedding as Embedding)
             ids.push('id'+index)
             index++
         }
         return embeddings
     }
-
 }
