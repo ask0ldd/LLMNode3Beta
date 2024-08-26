6be4d52c871ef1431d07da6d0b4b895ae3a860ae
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-08-26 06:34:00 +0200

    *edit* RAGStore : fixing the file to chroma collection process

--- AIRAGStore.ts
+++ AIRAGStore.ts
@@ -1,30 +1,30 @@
 import * as fs from "fs"
 import { ChromaClient, Collection, Embedding } from "chromadb"
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter"
 import { AIModel } from "./AIModel"
+import { AIEmbeddingModel } from "./AIEmbeddingModel"
 
 export class AIRAGStore{
 
     #chromaClient : ChromaClient = new ChromaClient()
-    #chromaCollections : Collection[]= []
+    #collections : Collection[]= []
 
     constructor() {
         return this
     }
 
     async newCollection(collectionName : string) {
-        if(this.#chromaCollections.find(collection => collection.name == collectionName)) throw new Error(`Collection ${collectionName} already exists`)
+        if(this.#collections.find(collection => collection.name == collectionName)) throw new Error(`Collection ${collectionName} already exists`)
         const collection = await this.#chromaClient.createCollection({name : collectionName, metadata: { "hnsw:space": "cosine" },})
-        this.#chromaCollections.push(collection)
+        this.#collections.push(collection)
     }
 
-    async addToCollection(collectionName : string, embeddingObj : { ids : string[], embeddings : Embedding[], documentChunks : string[]}) {
-        const collection = this.#chromaCollections.find(collection => collection.name == collectionName)
+    async addToCollection(collectionName : string, RAGTarget : { ids : string[], embeddings : Embedding[], documentChunks : string[]}) {
+        const collection = this.#collections.find(collection => collection.name == collectionName)
         if(!collection) throw new Error(`Collection ${collectionName} does not exist`)
-        await collection.add({ids : embeddingObj.ids, embeddings: embeddingObj.embeddings, documents: embeddingObj.documentChunks})
+        await collection.add({ids : RAGTarget.ids, embeddings: RAGTarget.embeddings, documents: RAGTarget.documentChunks})
     }
 
-    splitTextBlockToEmbeddableChunks(text : string, seqLength : number) : string[] {
+    splitTextBlockIntoChunks(text : string, seqLength : number) : string[] {
         const words = text.split(/\s+/)
         let sequences = []
         let sequence = []
@@ -39,27 +39,28 @@ export class AIRAGStore{
         return sequences
     }
 
-    splitUtf8TxtFileToEmbeddableChunks(filePath : string, seqLength  : number) : string[]{
+    splitUtf8TextFileIntoChunks(filePath : string, seqLength  : number) : string[]{
         try{
-            const file = fs.readFileSync(filePath, "utf8")
-            return this.splitTextBlockToEmbeddableChunks(file, 200)
+            const fileContent = fs.readFileSync(filePath, "utf8")
+            return this.splitTextBlockIntoChunks(fileContent, 200)
         }catch(error: any){
             console.log(error)
             return []
         }
     }
 
-    async splitAndEmbedUtf8TxtFile(filePath : string, model : AIModel, seqLength = 200) : Promise<Embedding[]>{
-        const chunks = this.splitUtf8TxtFileToEmbeddableChunks(filePath, seqLength)
+    async storeUtf8TextFileInACollection(collectionName : string, textFilePath : string, seqLength = 200) : Promise<Embedding[]>{
+        const chunks = this.splitUtf8TextFileIntoChunks(textFilePath, seqLength)
         let embeddings = []
         let ids = []
         let index = 1
         for(const chunk of chunks){
-            const response = await model.embeddings(chunk)
-            embeddings.push(response.embedding as Embedding)
+            const response = await AIEmbeddingModel.process(chunk)
+            embeddings.push(response.embedding)
             ids.push('id'+index)
             index++
         }
-        return embeddings
+        const RAGTarget = {ids, embeddings, documentChunks: chunks}
+        this.addToCollection(collectionName, RAGTarget)
     }
 }
