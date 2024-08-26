fa1b3afb230b44652a02c1af94fa350fc9e363d2
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-08-26 02:10:57 +0200

    *edit* AIRagTools : now handling a file from spliting to embedding

--- AIRagTools.ts
+++ AIRagTools.ts
@@ -1,10 +1,11 @@
 import * as fs from "fs"
 import { ChromaClient } from "chromadb"
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter"
+import { AIModel } from "./AIModel"
 
 export class AIRAGTools{
 
-    static splitStringToSequences(text : string, seqLength : number) : string[] {
+    static splitStringToEmbeddableChunks(text : string, seqLength : number) : string[] {
         const words = text.split(/\s+/)
         let sequences = []
         let sequence = []
@@ -19,14 +20,28 @@ export class AIRAGTools{
         return sequences
     }
 
-    static splitUtf8TxtFileToSequences(filePath : string, seqLength  : number) : string[]{
+    static splitUtf8TxtFileToEmbeddableChunks(filePath : string, seqLength  : number) : string[]{
         try{
             const file = fs.readFileSync(filePath, "utf8")
-            return this.splitStringToSequences(file, 200)
+            return this.splitStringToEmbeddableChunks(file, 200)
         }catch(error: any){
             console.log(error)
             return []
         }
+    }
+
+    static async splitAndEmbedUtf8TxtFile(filePath : string, model : AIModel, seqLength = 200){
+        const chunks = this.splitUtf8TxtFileToEmbeddableChunks(filePath, seqLength)
+        let embeddings = []
+        let ids = []
+        let index = 1
+        for(const chunk of chunks){
+            const response = await model.embeddings(chunk)
+            embeddings.push(response.embedding)
+            ids.push('id'+index)
+            index++
+        }
+        return embeddings
     }
 
 }
